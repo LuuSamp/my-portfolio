769667ae52cee04895d18d342b222ed4494c3fa2
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-03-20 12:27:40 -0300

    Starting with Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<!-- <h1> (Heading 1) represents the main heading of a webpage or a section.-->
+<h1> LUCIANO PEREIRA SAMPAIO </h1>
+
+<!-- <nav> (Navigation) is used to create a menu with links to other pages or external sites. -->
+    <!-- <nav>
+        <a href="index.html">Home</a>
+        <a href="./projects/">Projects</a>
+        <a href="./contact/">Contact</a>
+
+    </nav> -->
+
+<!-- <p> (Paragraph) represents a paragraph of text.-->
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque felis ante, finibus eget libero et, commodo lacinia nulla. Mauris mi felis, maximus a pulvinar at, mollis ac enim. Integer accumsan leo non tristique ullamcorper. Aenean facilisis dui libero, sagittis luctus diam placerat non. Mauris libero massa, ullamcorper eget ultrices vitae, hendrerit a ipsum. Quisque mi neque, convallis condimentum sagittis nec, luctus id ligula. Aliquam tincidunt molestie odio vel venenatis. Morbi quis turpis diam. Donec non rhoncus tellus, at varius lacus. </p>
+
+<!-- <img> (Image) tag is used to display images.-->
+<img src="images/my_image.jpeg" alt="Not nice AI 'dog'" width="700" height="600">
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<h1> CONTACT </h1>
+
+
+<form action="mailto:luciano.sampaio2004@gmail.com" method="get">
+    <label for="email">Email:</label>
+    <input type="email" name="email" placeholder="Your email">
+
+    <label for="subject">Subject:</label>
+    <input type="text" name="subject" placeholder="Subject">
+
+    <label for="body">Message:</label>
+    <textarea name="body" placeholder="Your message"></textarea>
+    
+    <button type="submit" id="button">Send</button>
+</form>
